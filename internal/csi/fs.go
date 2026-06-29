3484a510f9598a2c2386a6794b57f1466cac58d5
federicolepera <federico.lepera@kiratech.it>
2026-06-29 14:50:10 +0200

    feat: add cache completeness check in NodePublishVolume and corresponding tests

--- internal/csi/fs.go
+++ internal/csi/fs.go
@@ -1,11 +1,27 @@
 package csi
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
+
+const completeFileName = ".praesto-complete"
 
 func directoryExists(path string) (bool, error) {
 	info, err := os.Stat(path)
 	if err == nil {
 		return info.IsDir(), nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func cacheComplete(path string) (bool, error) {
+	info, err := os.Stat(filepath.Join(path, completeFileName))
+	if err == nil {
+		return !info.IsDir(), nil
 	}
 	if os.IsNotExist(err) {
 		return false, nil
